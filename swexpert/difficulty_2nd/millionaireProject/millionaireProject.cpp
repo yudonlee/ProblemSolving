d44089324bd7f00bf4fe609ce9835d6b3a1c7499
yudonlee <future0159@naver.com>
2020-11-06 17:19:39 +0900

    Sw Expert Academy #1859 millonaire Projects is solved. I make the mistake for declaring results.the results is more than billion.so i store the result with long long

--- swexpert/difficulty_2nd/millionaireProject/millionaireProject.cpp
+++ swexpert/difficulty_2nd/millionaireProject/millionaireProject.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int solution(int* n,int count);
+long long solution(int* n,int count);
 int main(){
     int count,number;
     cin >>number;
     for(int i=0; i<number;i++){
         cin >>count;
-        cout << "segv"<<endl;
         int *n = new int[count+1];
         for(int j=0;j<count;j++){
             cin >> n[j];
         }
-        int result = solution(n,count);
+        long long result = solution(n,count);
         cout <<"#"<<i+1<<" "<<result<<endl;
     }
     return 0;
@@ -20,7 +19,7 @@ int main(){
 int solution(int* n,int count){
     vector<int> point;
     int max = n[count-1];
-    int profit = 0;
+    long long profit = 0;
     int index = 0;
     for(int i=count-1;i>=0;i--){
         if(max <= n[i]){
